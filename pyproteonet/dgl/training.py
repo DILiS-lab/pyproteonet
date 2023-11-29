WIP Try fixing peptide masking bug, as well as 'hidden' label in dgl graph but seems to have adverse effects:

pyproteonet/dgl/training.py
@@ -31,8 +31,8 @@ def train_node_regression(model: torch.nn.Module, train_data_set: GraphDataSet, 
             abundances = graph.nodes['molecule'].data['abundance']
             train_nodes = graph.ndata['mask']
             features_train = features.clone()
-            if 'hide' in graph.ndata:
-                to_hide = graph.ndata['hide']
+            if 'hidden' in graph.ndata:
+                to_hide = graph.ndata['hidden']
                 features_train[to_hide, 0] = hide_substitute_value
             features_train[train_nodes, 0] = mask_substitute_value
             features_train[features_train.isnan()] = nan_substitute_value
@@ -67,8 +67,8 @@ def train_node_regression(model: torch.nn.Module, train_data_set: GraphDataSet, 
                     abundances = graph.nodes['molecule'].data['abundance']
                     mask_nodes = graph.nodes['molecule'].data['mask']
                     features_test = features.clone()
-                    if 'hide' in graph.nodes['molecule'].data:
-                        to_hide = graph.nodes['molecule'].data['hide']
+                    if 'hidden' in graph.nodes['molecule'].data:
+                        to_hide = graph.nodes['molecule'].data['hidden']
                         features_test[to_hide, 0] = hide_substitute_value
                     features_test[mask_nodes, 0] = mask_substitute_value
                     features_test[features_test.isnan()] = nan_substitute_value

pyproteonet/imputation/dnn/all_sample_gnn.py
@@ -10,7 +10,11 @@ from pyproteonet.lightning import ConsoleLogger
 from dgl.dataloading import GraphCollator
 
 from ...data.dataset import Dataset
-from ...masking.train_eval import train_eval_protein_and_mapped, train_eval_full_protein_and_mapped
+from ...masking.train_eval import (
+    train_eval_protein_and_mapped,
+    train_eval_full_protein_and_mapped,
+    train_eval_full_protein_and_mapped_backup
+)
 from ...normalization.dnn_normalizer import DnnNormalizer
 from ...dgl.collate import (
     masked_dataset_to_homogeneous_graph,
@@ -22,14 +26,13 @@ from ...masking.masking import mask_missing
 
 def impute_all_sample_gnn(
     dataset: Dataset,
-    protein_abundance_column: str,
-    peptide_abundance_column: str,
+    molecule: str,
+    mapping: str,
+    column: str,
+    partner_column: str,
     validation_fraction=0.1,
-    training_fraction=0.5,
-    peptide_masking_fraction=0.5,
-    protein_molecule="protein",
-    peptide_molecule="peptide",
-    mapping: Optional[str] = None,
+    training_fraction=0.3,
+    partner_masking_fraction=0.5,
     result_column: Optional[str] = None,
     max_epochs: int = 10000,
     validation_frequency: int = 10,
@@ -41,55 +44,58 @@ def impute_all_sample_gnn(
     logger: Optional[object] = None,
     log_every_n_steps: int = 10,
     protein_gt_column: Optional[str] = None,
+    max_partner_mnar_quantile: float = 0.0,
 ):
-    if mapping is None:
-        protein_molecule, mapping, peptide_molecule = dataset.infer_mapping(
-            molecule=protein_molecule, mapping=peptide_molecule
-        )
+    molecule, mapping, peptide_molecule = dataset.infer_mapping(
+        molecule=molecule, mapping=mapping
+    )
 
     in_dataset = dataset.copy(
         columns={
-            protein_molecule: [protein_abundance_column]
+            molecule: [column]
             if protein_gt_column is None
-            else [protein_abundance_column, protein_gt_column],
-            peptide_molecule: [peptide_abundance_column],
+            else [column, protein_gt_column],
+            peptide_molecule: [partner_column],
         }
     )
     in_dataset.rename_columns(
-        columns={protein_abundance_column: "abundance"},
-        molecules=[protein_molecule],
+        columns={column: "abundance"},
+        molecules=[molecule],
         inplace=True,
     )
     in_dataset.rename_columns(
-        columns={peptide_abundance_column: "abundance"},
+        columns={partner_column: "abundance"},
         molecules=[peptide_molecule],
         inplace=True,
     )
     if protein_gt_column is not None:
         in_dataset.rename_columns(
             columns={protein_gt_column: "abundance_gt"},
-            molecules=[protein_molecule],
+            molecules=[molecule],
             inplace=True,
         )
-        in_dataset.values[peptide_molecule]["abundance_gt"] = in_dataset.values[peptide_molecule]["abundance"]
-        non_missing = in_dataset.values[protein_molecule]["abundance"]
-        gt = in_dataset.values[protein_molecule]["abundance_gt"]
+        in_dataset.values[peptide_molecule]["abundance_gt"] = in_dataset.values[
+            peptide_molecule
+        ]["abundance"]
+        non_missing = in_dataset.values[molecule]["abundance"]
+        gt = in_dataset.values[molecule]["abundance_gt"]
         mask = ~non_missing.isna()
         gt[mask] = non_missing[mask]
-        in_dataset.values[protein_molecule]["abundance_gt"] = gt
+        in_dataset.values[molecule]["abundance_gt"] = gt
     normalizer = DnnNormalizer(columns=["abundance", "abundance_gt"])
     normalizer.normalize(dataset=in_dataset, inplace=True)
 
-    train_ds, eval_ds = train_eval_full_protein_and_mapped(
+    train_ds, eval_ds = train_eval_full_protein_and_mapped_backup(
         dataset=in_dataset,
         protein_abundance_column="abundance",
         peptide_abundance_column="abundance",
         validation_fraction=validation_fraction,
         training_fraction=training_fraction,
-        peptide_masking_fraction=peptide_masking_fraction,
-        protein_molecule=protein_molecule,
+        peptide_masking_fraction=partner_masking_fraction,
+        protein_molecule=molecule,
         mapping=peptide_molecule,
         train_mapped=train_on_mapped,
+        max_mnar_quantile=max_partner_mnar_quantile,
     )
 
     collator = GraphCollator()
@@ -113,9 +119,7 @@ def impute_all_sample_gnn(
                 features=[],
             )
         )
-        gt_ds = mask_missing(
-            dataset=in_dataset, molecule=protein_molecule, column="abundance"
-        )
+        gt_ds = mask_missing(dataset=in_dataset, molecule=molecule, column="abundance")
         gt_dl = DataLoader([gt_ds], batch_size=1, collate_fn=gt_collate_fn)
         eval_dl = [eval_dl, gt_dl]
         early_stopping_monitor = "validation_loss/dataloader_idx_0"
@@ -123,7 +127,7 @@ def impute_all_sample_gnn(
     num_samples = len(in_dataset.sample_names)
     # heads = [num_samples, num_samples]  # [num_samples]
     # dimensions = [8 * num_smples, 4*num_samples, 2*num_samples]
-    heads = [4*num_samples, 4*num_samples, 4*num_samples]
+    heads = [4 * num_samples, 4 * num_samples, 4 * num_samples]
     dimensions = [8, 8, 4]  # [1]#, num_samples, num_samples]
     print(heads)
     print(dimensions)
@@ -141,34 +145,38 @@ def impute_all_sample_gnn(
         nan_substitute_value=missing_substitute_value,
         out_dim=num_samples,
         use_gatv2=use_gatv2,
-        initial_dense_layers=[8 * num_samples, 2 * num_samples],#[8 * num_samples, 2 * num_samples]
+        initial_dense_layers=[
+            8 * num_samples,
+            2 * num_samples,
+        ],  # [8 * num_samples, 2 * num_samples]
         dropout=0.2,
         lr=0.0005,
     )
 
     if logger is None:
         logger = ConsoleLogger()
-    trainer = Trainer(
-        logger=logger,
-        max_epochs=max_epochs,
-        enable_checkpointing=False,
-        check_val_every_n_epoch=validation_frequency,
-        log_every_n_steps=log_every_n_steps,
-        callbacks=[
-            EarlyStopping(
-                monitor=early_stopping_monitor,
-                mode="min",
-                patience=early_stopping_patience,
-            )
-        ],
-    )
     module.uncertainty_loss = False
-    trainer.fit(module, train_dataloaders=train_dl, val_dataloaders=eval_dl)
-    if uncertainty_column is not None:
+    if uncertainty_column is None:
+        trainer = Trainer(
+            logger=logger,
+            max_epochs=max_epochs,
+            enable_checkpointing=False,
+            check_val_every_n_epoch=validation_frequency,
+            log_every_n_steps=log_every_n_steps,
+            callbacks=[
+                EarlyStopping(
+                    monitor=early_stopping_monitor,
+                    mode="min",
+                    patience=early_stopping_patience,
+                )
+            ],
+        )
+        trainer.fit(module, train_dataloaders=train_dl, val_dataloaders=eval_dl)
+    else:
         module.uncertainty_loss = True
         module.lr = 0.0001
         trainer = Trainer(
-            logger=ConsoleLogger(),
+            logger=logger,
             max_epochs=max_epochs,
             enable_checkpointing=False,
             check_val_every_n_epoch=validation_frequency,
@@ -183,9 +191,7 @@ def impute_all_sample_gnn(
         )
         trainer.fit(module, train_dataloaders=train_dl, val_dataloaders=eval_dl)
 
-    predict_ds = mask_missing(
-        dataset=in_dataset, molecule=protein_molecule, column="abundance"
-    )
+    predict_ds = mask_missing(dataset=in_dataset, molecule=molecule, column="abundance")
     predict_graph = predict_ds.to_dgl_graph(
         molecule_features={
             mol: ["abundance"] for mol in predict_ds.dataset.molecules.keys()
@@ -196,7 +202,7 @@ def impute_all_sample_gnn(
     predict_graph = masked_heterograph_to_homogeneous(
         masked_heterographs=[predict_graph], target="abundance", features=[]
     )[0]
-    protein_index = ntypes.index(protein_molecule)
+    protein_index = ntypes.index(molecule)
     protein_mask = (predict_graph.ndata[dgl.NTYPE] == protein_index).type(torch.bool)
     res = trainer.predict(
         module, DataLoader([predict_graph], batch_size=1, collate_fn=collator.collate)
@@ -206,27 +212,25 @@ def impute_all_sample_gnn(
     protein_ids = predict_graph.ndata[dgl.NID][protein_mask].type(torch.int64)
     prot_res = prot_res[protein_ids, :]
     masked_proteins = masked_proteins[protein_ids, :]
-    mat_pd = in_dataset.get_samples_value_matrix(
-        molecule=protein_molecule, column="abundance"
-    )
+    mat_pd = in_dataset.get_samples_value_matrix(molecule=molecule, column="abundance")
     mat = mat_pd.to_numpy()
     mat[masked_proteins.numpy()] = prot_res[masked_proteins].numpy()[:, 0]
     # mat[:, :] = prot_res[:, :].numpy()
     mat_pd.loc[:, :] = mat
     in_dataset.set_samples_value_matrix(
-        matrix=mat_pd, molecule=protein_molecule, column="abundance"
+        matrix=mat_pd, molecule=molecule, column="abundance"
     )
     normalizer.unnormalize(dataset=in_dataset, inplace=True)
-    vals = dataset.values[protein_molecule][protein_abundance_column]
-    res_vals = in_dataset.values[protein_molecule]["abundance"]
+    vals = dataset.values[molecule][column]
+    res_vals = in_dataset.values[molecule]["abundance"]
     vals.loc[vals.isna(), :] = res_vals.loc[vals.isna(), :]
     if result_column is not None:
-        dataset.values[protein_molecule][result_column] = vals
+        dataset.values[molecule][result_column] = vals
     if uncertainty_column is not None:
         mat[:, :] = np.nan
         mat[masked_proteins.numpy()] = prot_res[masked_proteins].numpy()[:, 1]
         mat_pd.loc[:, :] = mat
         dataset.set_samples_value_matrix(
-            matrix=mat_pd, molecule=protein_molecule, column=uncertainty_column
+            matrix=mat_pd, molecule=molecule, column=uncertainty_column
         )
     return vals

pyproteonet/lightning/abstract_node_imputer.py
@@ -53,8 +53,8 @@ class AbstractNodeImputer(pl.LightningModule):
         target = graph.ndata["target"].float().clone()
         features = graph.ndata["features"].float()
         mask_nodes = graph.ndata["mask"]
-        if "hide" in graph.ndata:
-            to_hide = graph.ndata["hide"]
+        if "hidden" in graph.ndata:
+            to_hide = graph.ndata["hidden"]
             #if len(to_hide.shape) == 1:
             #    to_hide = torch.unsqueeze(to_hide, dim=-1)
             target[to_hide] = self.hide_substitute_value

pyproteonet/lightning/uncertainty_gat_node_imputer.py
@@ -154,7 +154,7 @@ class UncertaintyGatNodeImputer(AbstractNodeImputer):
 
     def calculate_loss(self, pred, target):
         if self.uncertainty_loss:
-            return F.gaussian_nll_loss(pred[:, 0], target=target, var=torch.abs(pred[:, 1]), eps=1e-1)
+            return F.gaussian_nll_loss(pred[:, 0], target=target, var=torch.exp(pred[:, 1]))
         else:
             return F.mse_loss(pred[:, 0], target)
 

pyproteonet/masking/train_eval.py
@@ -1,6 +1,9 @@
 from typing import Tuple
 import random
 
+import numpy as np
+import pandas as pd
+
 from ..data.dataset import Dataset
 from ..data.masked_dataset import MaskedDataset
 from .masked_dataset_generator import MaskedDatasetGenerator
@@ -35,7 +38,7 @@ def train_eval_protein_and_mapped(
     mapping = dataset.mappings[mapping].df
     if train_mapped:
         validation_peptides = (
-            mapping[mapping.index.get_level_values(0).isin(validation_proteins)]
+            mapping[mapping.index.get_level_values(protein_molecule).isin(validation_proteins)]
             .sample(frac=validation_fraction)
             .index.get_level_values(1)
         )
@@ -83,7 +86,7 @@ def train_eval_protein_and_mapped(
             prot_mask[sample] = False
             prot_mask.loc[ids, sample] = True
             relevant_peptides[sample] = mapping[
-                mapping.index.get_level_values(0).isin(ids)
+                mapping.index.get_level_values(protein_molecule).isin(ids)
             ].index.get_level_values(1)
         pep_mask = input_dataset.get_samples_value_matrix(
             molecule=peptide_molecule, column=peptide_abundance_column
@@ -118,7 +121,8 @@ def train_eval_protein_and_mapped(
 
     return MaskedDatasetGenerator(datasets=[dataset], generator_fn=mask), eval_dataset
 
-def train_eval_full_protein_and_mapped(
+
+def train_eval_full_protein_and_mapped_backup(
     dataset: Dataset,
     protein_abundance_column: str,
     peptide_abundance_column: str,
@@ -128,6 +132,7 @@ def train_eval_full_protein_and_mapped(
     protein_molecule="protein",
     mapping="peptide",
     train_mapped: bool = False,
+    max_mnar_quantile: float = 0,
 ) -> Tuple[MaskedDatasetGenerator, MaskedDataset]:
     protein_molecule, mapping, peptide_molecule = dataset.infer_mapping(
         molecule=protein_molecule, mapping=mapping
@@ -207,6 +212,268 @@ def train_eval_full_protein_and_mapped(
             ids = ids.sample(frac=pep_frac).index
             pep_mask[sample_name] = False
             pep_mask.loc[ids, sample_name] = True
+        if train_mapped:
+            return MaskedDataset(
+                dataset=dataset,
+                masks={protein_molecule: prot_mask, peptide_molecule: pep_mask},
+                hidden={
+                    protein_molecule: eval_protein_mask,
+                    peptide_molecule: eval_peptide_mask,
+                },
+            )
+        else:
+            return MaskedDataset(
+                dataset=dataset,
+                masks={protein_molecule: prot_mask},
+                hidden={
+                    protein_molecule: eval_protein_mask,
+                    peptide_molecule: pep_mask,
+                },
+            )
+
+    return MaskedDatasetGenerator(datasets=[dataset], generator_fn=mask), eval_dataset
+
+
+def train_eval_full_protein_and_mapped(
+    dataset: Dataset,
+    protein_abundance_column: str,
+    peptide_abundance_column: str,
+    validation_fraction=0.2,
+    training_fraction=0.1,
+    peptide_masking_fraction=0.5,
+    protein_molecule="protein",
+    mapping="peptide",
+    train_mapped: bool = False,
+    max_mnar_quantile = 0
+) -> Tuple[MaskedDatasetGenerator, MaskedDataset]:
+    protein_molecule, mapping, peptide_molecule = dataset.infer_mapping(
+        molecule=protein_molecule, mapping=mapping
+    )
+    validation_proteins = (
+        dataset.molecules[protein_molecule].sample(frac=validation_fraction).index
+    )
+    eval_protein_mask = mask_non_missing(
+        dataset=dataset,
+        molecule=protein_molecule,
+        column=protein_abundance_column,
+        ids=validation_proteins,
+        frac=None,
+    )
+    eval_protein_mask = eval_protein_mask.masks[protein_molecule]
+    mapping = dataset.mappings[mapping].df
+    if train_mapped:
+        validation_peptides = (
+            mapping[mapping.index.get_level_values(protein_molecule).isin(validation_proteins)]
+            .sample(frac=validation_fraction)
+            .index.get_level_values(peptide_molecule)
+        )
+        eval_peptide_mask = mask_non_missing(
+            dataset=dataset,
+            molecule=peptide_molecule,
+            column=peptide_abundance_column,
+            ids=validation_peptides,
+            frac=None,
+        )
+        eval_peptide_mask = eval_peptide_mask.masks[peptide_molecule]
+        eval_dataset = MaskedDataset(
+            dataset=dataset,
+            masks={
+                protein_molecule: eval_protein_mask,
+                peptide_molecule: eval_peptide_mask,
+            },
+        )
+    else:
+        eval_dataset = MaskedDataset(
+            dataset=dataset, masks={protein_molecule: eval_protein_mask}
+        )
+
+    available_prots = dataset.get_samples_value_matrix(molecule=protein_molecule, column=protein_abundance_column)
+    available_prots = (~available_prots.isna()).sum(axis=1)
+    available_prots = available_prots[(available_prots>0) & ~available_prots.index.isin(validation_proteins)]
+    rng = np.random.default_rng()
+
+    def mask(input_dataset: Dataset):
+        prot_mask = input_dataset.get_samples_value_matrix(
+            molecule=protein_molecule, column=protein_abundance_column
+        )
+        if isinstance(training_fraction, tuple):
+            train_frac = random.uniform(training_fraction[0], training_fraction[1])
+        else:
+            train_frac = training_fraction
+        if isinstance(peptide_masking_fraction, tuple):
+            pep_frac = random.uniform(peptide_masking_fraction[0], peptide_masking_fraction[1])
+        else:
+            pep_frac = peptide_masking_fraction
+
+        # num_bins = 10
+        # prot_mean = prot_mask.loc[available_prots.index].mean(axis=1)
+        # prot_bin = pd.cut(prot_mean, bins=num_bins, labels=False)
+        # masked_prots = prot_mean.groupby(prot_bin).apply(lambda x: x.sample(frac=train_frac)).index.get_level_values(1)
+        masked_prots = available_prots.sample(frac=train_frac).index
+        relevant_peptides = mapping[
+            mapping.index.get_level_values(protein_molecule).isin(masked_prots)
+        ].index.get_level_values(peptide_molecule)
+        for sample_name, sample in input_dataset.samples_dict.items():
+            ids = sample.values[protein_molecule].loc[masked_prots, protein_abundance_column]
+            ids = ids[~ids.isna()].index
+            prot_mask[sample_name] = False
+            prot_mask.loc[ids, sample_name] = True
+        pep_mask = input_dataset.get_samples_value_matrix(
+            molecule=peptide_molecule, column=peptide_abundance_column
+        )
+        pep_vals = pep_mask.copy()
+        mnar_quantile = rng.uniform(0, max_mnar_quantile)
+        mnar_quantile_val = np.nanquantile(pep_mask.values, mnar_quantile)
+        mnar_std = mnar_quantile_val - np.nanquantile(pep_mask.values, mnar_quantile - mnar_quantile / 4)
+        for sample_name in input_dataset.sample_names:
+            ids = pep_mask[sample_name].isna()
+            ids = ids[~ids]
+            ids = ids[ids.index.isin(relevant_peptides)]
+            if train_mapped:
+                ids = ids[~ids.index.isin(validation_peptides)]
+            ids_mcar = ids.sample(frac=pep_frac).index
+            pep_mask[sample_name] = False
+            pep_mask.loc[ids_mcar, sample_name] = True
+            if max_mnar_quantile > 0:
+                thresh = rng.normal(loc=mnar_quantile_val, scale=mnar_std, size=ids.shape[0])
+                vals = pep_vals.loc[ids.index, sample_name]
+                ids_mnar = vals.loc[vals < thresh].index
+                pep_mask.loc[ids_mnar, sample_name] = True
+        if train_mapped:
+            return MaskedDataset(
+                dataset=dataset,
+                masks={protein_molecule: prot_mask, peptide_molecule: pep_mask},
+                hidden={
+                    protein_molecule: eval_protein_mask,
+                    peptide_molecule: eval_peptide_mask,
+                },
+            )
+        else:
+            return MaskedDataset(
+                dataset=dataset,
+                masks={protein_molecule: prot_mask},
+                hidden={
+                    protein_molecule: eval_protein_mask,
+                    peptide_molecule: pep_mask,
+                },
+            )
+
+    return MaskedDatasetGenerator(datasets=[dataset], generator_fn=mask), eval_dataset
+
+def train_eval_full_protein_and_mapped_new(
+    dataset: Dataset,
+    protein_abundance_column: str,
+    peptide_abundance_column: str,
+    validation_fraction=0.2,
+    training_fraction=0.1,
+    peptide_masking_fraction=0.5,
+    protein_molecule="protein",
+    mapping="peptide",
+    train_mapped: bool = False,
+    max_mnar_quantile: float = 0,
+) -> Tuple[MaskedDatasetGenerator, MaskedDataset]:
+    protein_molecule, mapping, peptide_molecule = dataset.infer_mapping(
+        molecule=protein_molecule, mapping=mapping
+    )
+    assert protein_molecule != peptide_molecule
+    validation_proteins = (
+        dataset.molecules[protein_molecule].sample(frac=validation_fraction).index
+    )
+    eval_protein_mask = mask_non_missing(
+        dataset=dataset,
+        molecule=protein_molecule,
+        column=protein_abundance_column,
+        ids=validation_proteins,
+        frac=None,
+    )
+    eval_protein_mask = eval_protein_mask.masks[protein_molecule]
+    mapping = dataset.mappings[mapping].df
+    if train_mapped:
+        validation_peptides = (
+            mapping[mapping.index.get_level_values(protein_molecule).isin(validation_proteins)]
+            .sample(frac=validation_fraction)
+            .index.get_level_values(peptide_molecule)
+        )
+        eval_peptide_mask = mask_non_missing(
+            dataset=dataset,
+            molecule=peptide_molecule,
+            column=peptide_abundance_column,
+            ids=validation_peptides,
+            frac=None,
+        )
+        eval_peptide_mask = eval_peptide_mask.masks[peptide_molecule]
+        eval_dataset = MaskedDataset(
+            dataset=dataset,
+            masks={
+                protein_molecule: eval_protein_mask,
+                peptide_molecule: eval_peptide_mask,
+            },
+        )
+    else:
+        eval_dataset = MaskedDataset(
+            dataset=dataset, masks={protein_molecule: eval_protein_mask}
+        )
+
+    available_prots = dataset.get_samples_value_matrix(molecule=protein_molecule, column=protein_abundance_column)
+    available_prots = (~available_prots.isna()).sum(axis=1)
+    available_prots = available_prots[(available_prots>0) & ~available_prots.index.isin(validation_proteins)]
+
+
+    rng = np.random.default_rng()
+
+    def mask(input_dataset: Dataset):
+        prot_mask = input_dataset.get_samples_value_matrix(
+            molecule=protein_molecule, column=protein_abundance_column
+        )
+        if isinstance(training_fraction, tuple):
+            train_frac = random.uniform(training_fraction[0], training_fraction[1])
+        else:
+            train_frac = training_fraction
+        if isinstance(peptide_masking_fraction, tuple):
+            pep_frac = random.uniform(peptide_masking_fraction[0], peptide_masking_fraction[1])
+        else:
+            pep_frac = peptide_masking_fraction
+        
+        num_bins = 10
+        prot_mean = prot_mask.loc[available_prots.index].mean(axis=1)
+        prot_mean = prot_mean[prot_mean < prot_mean.quantile(0.5)]
+        prot_bin = pd.cut(prot_mean, bins=num_bins, labels=False)
+        masked_prots = prot_mean.groupby(prot_bin).apply(lambda x: x.sample(frac=train_frac)).index.get_level_values(1)
+        #masked_prots = prot_mask.loc[available_prots.index].sample(frac=train_frac).index
+        relevant_peptides = mapping[
+            mapping.index.get_level_values(protein_molecule).isin(masked_prots)
+        ].index.get_level_values(peptide_molecule)
+        for sample_name, sample in input_dataset.samples_dict.items():
+            ids = sample.values[protein_molecule].loc[masked_prots, protein_abundance_column]
+            ids = ids[~ids.isna()].index
+            prot_mask[sample_name] = False
+            prot_mask.loc[ids, sample_name] = True
+
+        pep_mask = input_dataset.get_samples_value_matrix(
+            molecule=peptide_molecule, column=peptide_abundance_column
+        )
+        pep_vals = pep_mask.copy()
+        mnar_quantile = max_mnar_quantile#rng.uniform(0, max_mnar_quantile)
+        mnar_quantile_val = np.nanquantile(pep_mask.values, mnar_quantile)
+        mnar_std = mnar_quantile_val - np.nanquantile(pep_mask.values, mnar_quantile - mnar_quantile / 4)
+        for sample_name in input_dataset.sample_names:
+            ids = pep_mask[sample_name].isna()
+            ids = ids[~ids]
+            ids = ids[ids.index.isin(relevant_peptides)]
+            if train_mapped:
+                ids = ids[~ids.index.isin(validation_peptides)]
+            ids_mcar = ids.sample(frac=pep_frac).index
+            pep_mask[sample_name] = False
+            pep_mask.loc[ids_mcar, sample_name] = True
+            if max_mnar_quantile > 0:
+                thresh = rng.normal(loc=mnar_quantile_val, scale=mnar_std, size=ids.shape[0])
+                vals = pep_vals.loc[ids.index, sample_name]
+                ids_mnar = vals.loc[vals < thresh].index
+                pep_mask.loc[ids_mnar, sample_name] = True
+        # print('pep_mask')
+        # print(pep_mask.sum())
+        # print('prot_mask')
+        # print(prot_mask.sum())
         if train_mapped:
             return MaskedDataset(
                 dataset=dataset,