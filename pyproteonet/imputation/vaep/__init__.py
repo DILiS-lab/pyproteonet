0023651e0fb149a020dd8a91f959f48ca55eb7e5
Tobias <pito.mailing@gmail.com>
2024-02-07 10:15:21 +0100

    Fix dependencies to fix problem with automatic building of documentation

--- pyproteonet/imputation/vaep/__init__.py
+++ pyproteonet/imputation/vaep/__init__.py
@@ -11,7 +11,7 @@ logging.getLogger(__name__).addHandler(NullHandler())
 
 # put into some pandas_cfg.py file and import all
 import pandas as pd
-import pandas.io.formats.format as pf
+#import pandas.io.formats.format as pf
 
 # import vaep.pandas
 # import vaep.plotting
@@ -27,14 +27,14 @@ __version__ = (0, 1, 0)
 
 
 # set some defaults
-class IntArrayFormatter(pf.GenericArrayFormatter):
-    def _format_strings(self):
-        formatter = self.formatter or '{:,d}'.format
-        fmt_values = [formatter(x) for x in self.values]
-        return fmt_values
+# class IntArrayFormatter(pf.GenericArrayFormatter):
+#     def _format_strings(self):
+#         formatter = self.formatter or '{:,d}'.format
+#         fmt_values = [formatter(x) for x in self.values]
+#         return fmt_values
 
 
 pd.options.display.float_format = '{:,.3f}'.format
-pf.IntArrayFormatter = IntArrayFormatter
+#pf.IntArrayFormatter = IntArrayFormatter
 
 make_large_descriptors('x-large')
